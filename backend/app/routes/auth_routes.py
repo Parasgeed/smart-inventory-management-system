cce8e7c80de8808a6346ff2d471095aedcd0f00a
Paras Geed <parasgeed48@gmail.com>
2026-06-16 16:10:15 +0530

    Implemented JWT authentication and protected routes

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -6,12 +6,19 @@ from app.models.user_model import User
 from app.schemas.user_schema import UserCreate
 from app.auth.security import hash_password
 
+from app.schemas.user_schema import UserLogin
+from app.auth.security import verify_password
+from app.auth.auth import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.auth.dependencies import get_current_user
+
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
 
-
+# User Registration
 @router.post("/register")
 def register_user(
     user: UserCreate,
@@ -42,4 +49,55 @@ def register_user(
     return {
         "message": "User registered successfully",
         "user_id": new_user.id
+    }
+
+# User Login
+@router.post("/login")
+def login_user(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == form_data.username
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        form_data.password,
+        existing_user.password_hash
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        {
+            "sub": existing_user.email,
+            "user_id": existing_user.id,
+            "role_id": existing_user.role_id
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+# Get Current User
+@router.get("/me")
+def get_me(
+    current_user = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
+        "role_id": current_user.role_id
     }
